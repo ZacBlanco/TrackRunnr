b799ff2e958d8a5e417739b89979d00819365702
Richard Ahn <richahn2@gmail.com>
2016-04-17 01:14:25 -0400

    Fixed the routing issues

--- app/routes.js
+++ app/routes.js
@@ -3,58 +3,46 @@ var userController = require('../controllers/userController');
 module.exports = function(app, passport, express) {
 
     // ====================== Normal Routing ========================//
-    app.get('/', isLoggedIn, function(req, res) {
+    app.get('/', function(req, res, next) {
+            isLoggedIn(req, res, next, false);
+        }, function(req, res) {
         res.render('pages/index.ejs', {
-            loggedIn: req.isAuthenticated(),
-            user: req.user
-        });
-    });
-    app.get('/login', function(req, res) {
-        res.render('pages/login.ejs');
-    });
-    app.post('/login', function(req, res, next) {
-        passport.authenticate('local-login', function(err, user, info) {
-            if (err) { return next(err); }
-            if (!user) { return res.redirect('/login'); }
-            req.logIn(user, function(err) {
-                if (err) { return next(err); }
-                return res.redirect('/');
+                loggedIn: req.isAuthenticated(),
+                user: req.user,
+                message: req.flash('loginMessage')
             });
-        }
-        , {
-            successRedirect: '/',
-            failureRedirect: '/',
-            failureFlash: true
-        })(req, res, next);
     });
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/',
+        failureRedirect: '/',
+        failureFlash: true
+    }))
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
     app.get('/signup', function(req, res) {
-        res.render('pages/signup.ejs');
-    });
-    app.post('/signup', function(req, res, next) {
-        passport.authenticate('local-signup', function(err, user, info) {
-            if (err) { return next(err); }
-            if (!user) { return res.redirect('/login'); }
-            req.logIn(user, function(err) {
-                if (err) { return next(err); }
-                return res.redirect('/');
-            });
-        }
-        , {
-            successRedirect: '/',
-            failureRedirect: '/',
-            failureFlash: true
-        })(req, res, next);
+        res.render('pages/signup.ejs', {
+            message: req.flash('signupMessage')
+        });
     });
 
-    function isLoggedIn(req, res, next) {
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }))
+
+
+    function isLoggedIn(req, res, next, shouldRedirect) {
         if (req.isAuthenticated()) {
             return next();
         }
-        res.redirect('/');
+        if (shouldRedirect) {
+            res.redirect(url);
+        } else {
+            return next();
+        }
     }
 
     // ====================== API Routing ========================//
@@ -65,7 +53,6 @@ module.exports = function(app, passport, express) {
     apiRouter.route('/users/:username')
         .delete(userController.deleteUser)
         .get(userController.getUser);
-
 
     // Register all Routers
     app.use('/api', apiRouter);
